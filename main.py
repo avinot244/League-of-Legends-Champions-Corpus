736c0ca7e09e81b7bcb96009335db547e09be10e
avionot244 <aymericvinot38@gmail.com>
2024-04-26 17:11:10 +0200

    Added the database

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ from packages.utils import saveToJson
 from packages.api_calls import get_champion_SnW, get_champion_powerSpikes
 
 
-import os
 import json
+from tqdm import tqdm
 
 if __name__ == "__main__":
 
@@ -15,41 +15,42 @@ if __name__ == "__main__":
     champion_names : list[str] = list(champion_mapping.keys())
 
     champion_names = [s.lower() for s in champion_names]
-    
-    test = champion_names[-1]
-    snw : dict = get_champion_SnW(test)
-    snwDataList : list = snw["data"]["guidesByRoleData"]
-    
-    powerSpikes : dict = get_champion_powerSpikes(test)
-    powerSpikesDataList : list = powerSpikes["data"]["powerSpikesData"]
 
     row_idx : int = 0
-    for pwData, snwData in zip(powerSpikesDataList, snwDataList):
-        # For Strenght and weaknesses
-        rowContentS : RowContent = RowContent(snwData["flatData"]["strengths"], -1)
-        rowS : Row = Row(row_idx, rowContentS)
-        row_idx += 1
-        saveToJson(rowS.asDict(), "./dataset.json")
+    for champion_name in tqdm(champion_names):
+        snw : dict = get_champion_SnW(champion_name)
+        snwDataList : list = snw["data"]["guidesByRoleData"]
+        
+        powerSpikes : dict = get_champion_powerSpikes(champion_name)
+        powerSpikesDataList : list = powerSpikes["data"]["powerSpikesData"]
 
-        rowContentW : RowContent = RowContent(snwData["flatData"]["weaknesses"], -1)
-        rowW : Row = Row(row_idx, rowContentW)
-        row_idx += 1
-        saveToJson(rowW.asDict(), "./dataset.json")
         
-        
-        # For Power spikes
-        pwGameStages = pwData["flatData"]["gameStages"]
-        for pwGS in pwGameStages:
-            
-            rowContentGamePlan : RowContent = RowContent(pwGS["gamePlan"], -1)
-            rowGamePlan : Row = Row(row_idx, rowContentGamePlan)
+        for pwData, snwData in zip(powerSpikesDataList, snwDataList):
+            # For Strenght and weaknesses
+            rowContentS : RowContent = RowContent(snwData["flatData"]["strengths"], -1)
+            rowS : Row = Row(row_idx, rowContentS)
             row_idx += 1
-            saveToJson(rowGamePlan.asDict(), "./dataset.json")
+            saveToJson(rowS.asDict(), "./dataset.json")
 
-            rowContentDescription : RowContent = RowContent(pwGS["powerSpikeDescription"], -1)
-            rowDescription : Row = Row(row_idx, rowContentDescription)
-            row_idx += 1           
-            saveToJson(rowDescription.asDict(), "./dataset.json")
+            rowContentW : RowContent = RowContent(snwData["flatData"]["weaknesses"], -1)
+            rowW : Row = Row(row_idx, rowContentW)
+            row_idx += 1
+            saveToJson(rowW.asDict(), "./dataset.json")
+            
+            
+            # For Power spikes
+            pwGameStages = pwData["flatData"]["gameStages"]
+            for pwGS in pwGameStages:
+                
+                rowContentGamePlan : RowContent = RowContent(pwGS["gamePlan"], -1)
+                rowGamePlan : Row = Row(row_idx, rowContentGamePlan)
+                row_idx += 1
+                saveToJson(rowGamePlan.asDict(), "./dataset.json")
+
+                rowContentDescription : RowContent = RowContent(pwGS["powerSpikeDescription"], -1)
+                rowDescription : Row = Row(row_idx, rowContentDescription)
+                row_idx += 1           
+                saveToJson(rowDescription.asDict(), "./dataset.json")
             
 
 
