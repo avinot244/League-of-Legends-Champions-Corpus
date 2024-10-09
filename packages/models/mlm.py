d9df9fb08a17dac7b93d00cbdfa95d8e6749a215
avinot244 <aymericvinot38@gmail.com>
2024-10-09 23:56:07 +0200

    addingmlm code

--- packages/models/mlm.py
+++ packages/models/mlm.py
@@ -1,59 +1,192 @@
-import os
 import torch
-from transformers import BertTokenizer, BertForMaskedLM, Trainer, TrainingArguments, DataCollatorForLanguageModeling
+from transformers import AutoModelForMaskedLM, AutoTokenizer, DataCollatorForLanguageModeling, default_data_collator, TrainingArguments, Trainer
+import torch
 from datasets import load_dataset
+import collections
+import numpy as np
+import math
 
-# Load the dataset
-dataset = load_dataset('avinot/LoL-Champions-Corpus')
+# source : https://huggingface.co/learn/nlp-course/chapter7/3
 
-# Load the pre-trained BERT tokenizer
-tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
+### Loading model
+model_checkpoint = "distilbert-base-uncased"
+model = AutoModelForMaskedLM.from_pretrained(model_checkpoint)
+print(model)
 
-# Tokenize the dataset
+### Loading tokenizer
+tokenizer = AutoTokenizer.from_pretrained(model_checkpoint)
+tokenizer.model_max_length
+
+### Small demonstration before training
+text = "Aatrox has a [MASK] early game"
+inputs = tokenizer(text, return_tensors="pt")
+token_logits = model(**inputs).logits
+# Find the location of [MASK] and extract its logits
+mask_token_index = torch.where(inputs["input_ids"] == tokenizer.mask_token_id)[1]
+mask_token_logits = token_logits[0, mask_token_index, :]
+# Pick the [MASK] candidates with the highest logits
+top_5_tokens = torch.topk(mask_token_logits, 5, dim=1).indices[0].tolist()
+
+for token in top_5_tokens:
+    print(f"'>>> {text.replace(tokenizer.mask_token, tokenizer.decode([token]))}'")
+    
+    
+### Loading dataset
+dataset = load_dataset("avinot/LoL-Champions-Corpus")
+print(dataset)
+
+sample = dataset["train"].shuffle(seed=42).select(range(3))
+for row in sample:
+    print(f"\n'>>> Review: {row['text']}'")
+    print(f"'>>> Label: {row['label']}'")
+    
+### Tokenizing dataset
 def tokenize_function(examples):
-    return tokenizer(examples['text'], padding='max_length', truncation=True, max_length=128)
+    result = tokenizer(examples["text"])
+    if tokenizer.is_fast:
+        result["word_ids"] = [result.word_ids(i) for i in range(len(result["input_ids"]))]
+    return result
 
-tokenized_datasets = dataset.map(tokenize_function, batched=True, remove_columns=['text'])
 
-# Data Collator for MLM (this will handle masking the tokens during training)
-data_collator = DataCollatorForLanguageModeling(
-    tokenizer=tokenizer,
-    mlm=True,
-    mlm_probability=0.15
+# Use batched=True to activate fast multithreading!
+tokenized_datasets = dataset.map(
+    tokenize_function, batched=True, remove_columns=["text", "label"]
 )
+print(tokenized_datasets)
 
-# Load the pre-trained BERT model
-model = BertForMaskedLM.from_pretrained('bert-base-uncased')
+chunk_size = 128
+tokenized_samples = tokenized_datasets["train"][:3]
+for idx, sample in enumerate(tokenized_samples["input_ids"]):
+    print(f"'>>> Corpus {idx} length: {len(sample)}'")
 
-# Define training arguments
+concatenated_examples = {
+    k: sum(tokenized_samples[k], []) for k in tokenized_samples.keys()
+}
+total_length = len(concatenated_examples["input_ids"])
+print(f"'>>> Concatenated corpus length: {total_length}'")
+
+
+def group_texts(examples):
+    # Concatenate all texts
+    concatenated_examples = {k: sum(examples[k], []) for k in examples.keys()}
+    # Compute length of concatenated texts
+    total_length = len(concatenated_examples[list(examples.keys())[0]])
+    # We drop the last chunk if it's smaller than chunk_size
+    total_length = (total_length // chunk_size) * chunk_size
+    # Split by chunks of max_len
+    result = {
+        k: [t[i : i + chunk_size] for i in range(0, total_length, chunk_size)]
+        for k, t in concatenated_examples.items()
+    }
+    # Create a new labels column
+    result["labels"] = result["input_ids"].copy()
+    return result
+
+lm_dataset = tokenized_datasets.map(group_texts, batched=True)
+print(lm_dataset)
+
+print(tokenizer.decode(lm_dataset["train"][1]["input_ids"]))
+print(tokenizer.decode(lm_dataset["train"][1]["labels"]))
+
+
+### Set up datacollator
+
+data_collator = DataCollatorForLanguageModeling(tokenizer=tokenizer, mlm_probability=0.15)
+samples = [lm_dataset["train"][i] for i in range(2)]
+for sample in samples:
+    _ = sample.pop("word_ids")
+
+for chunk in data_collator(samples)["input_ids"]:
+    print(f"\n'>>> {tokenizer.decode(chunk)}'")
+
+### Masking dataset
+wwm_probability = 0.2
+
+def whole_word_masking_data_collator(features):
+    for feature in features:
+        word_ids = feature.pop("word_ids")
+
+        # Create a map between words and corresponding token indices
+        mapping = collections.defaultdict(list)
+        current_word_index = -1
+        current_word = None
+        for idx, word_id in enumerate(word_ids):
+            if word_id is not None:
+                if word_id != current_word:
+                    current_word = word_id
+                    current_word_index += 1
+                mapping[current_word_index].append(idx)
+
+        # Randomly mask words
+        mask = np.random.binomial(1, wwm_probability, (len(mapping),))
+        input_ids = feature["input_ids"]
+        labels = feature["labels"]
+        new_labels = [-100] * len(labels)
+        for word_id in np.where(mask)[0]:
+            word_id = word_id.item()
+            for idx in mapping[word_id]:
+                new_labels[idx] = labels[idx]
+                input_ids[idx] = tokenizer.mask_token_id
+        feature["labels"] = new_labels
+
+    return default_data_collator(features)
+
+samples = [lm_dataset["train"][i] for i in range(2)]
+batch = whole_word_masking_data_collator(samples)
+
+for chunk in batch["input_ids"]:
+    print(f"\n'>>> {tokenizer.decode(chunk)}'")
+
+### Set up training
+batch_size = 64
+# Show the training loss with every epoch
+logging_steps = len(lm_dataset["train"]) // batch_size
+model_name = model_checkpoint.split("/")[-1]
+
+
 training_args = TrainingArguments(
-    output_dir='./bert-mlm-lol-champions',   # directory to save the model
+    "distilbert-lolchamps",
     overwrite_output_dir=True,
-    num_train_epochs=3,
-    per_device_train_batch_size=8,   # Adjust based on your GPU memory
-    per_device_eval_batch_size=8,
-    evaluation_strategy="steps",    # Evaluate at every N steps
-    logging_dir='./logs',           # Directory for logging
-    logging_steps=500,
-    save_steps=1000,
-    eval_steps=1000,
-    save_total_limit=2,             # Only last 2 checkpoints will be saved
-    prediction_loss_only=True,      # Simplifies output for MLM task
-    learning_rate=5e-5,
+    evaluation_strategy="epoch",
+    learning_rate=2e-5,
+    weight_decay=0.01,
+    per_device_train_batch_size=batch_size,
+    per_device_eval_batch_size=batch_size,
+    push_to_hub=True,
+    fp16=False,
+    logging_steps=logging_steps,
+    hub_token="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"
 )
 
-# Define the trainer
 trainer = Trainer(
     model=model,
     args=training_args,
-    train_dataset=tokenized_datasets['train'],
-    eval_dataset=tokenized_datasets['test'],   # Assuming dataset has 'test' split
+    train_dataset=lm_dataset["train"],
+    eval_dataset=lm_dataset["validation"],
     data_collator=data_collator,
+    tokenizer=tokenizer,
 )
 
-# Fine-tune the model
+### Training
+eval_results = trainer.evaluate()
+print(f">>> Perplexity: {math.exp(eval_results['eval_loss']):.2f}")
+
 trainer.train()
 
-# Save the final model
-model.save_pretrained('./bert-mlm-lol-champions-final')
-tokenizer.save_pretrained('./bert-mlm-lol-champions-final')
+
+### Results
+eval_results = trainer.evaluate()
+print(f">>> Perplexity: {math.exp(eval_results['eval_loss']):.2f}")
+
+test_text = "Aatrox has a [MASK] early game"
+
+inputs = tokenizer(test_text, return_tensors="pt")
+token_logits = model(**inputs).logits
+# Find the location of [MASK] and extract its logits
+mask_token_index = torch.where(inputs["input_ids"] == tokenizer.mask_token_id)[1]
+mask_token_logits = token_logits[0, mask_token_index, :]
+# Pick the [MASK] candidates with the highest logits
+top_5_tokens = torch.topk(mask_token_logits, 5, dim=1).indices[0].tolist()
+
+for token in top_5_tokens:
+    print(f"'>>> {test_text.replace(tokenizer.mask_token, tokenizer.decode([token]))}'")
