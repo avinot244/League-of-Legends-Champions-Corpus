ddd77478e1dfb21a6484e6d743a7957b9c015ebb
avinot244 <aymericvinot38@gmail.com>
2025-02-12 21:47:09 +0100

    Downloading more video audios and fixing label assignment

--- packages/db_manager/youtube/youtube_data.py
+++ packages/db_manager/youtube/youtube_data.py
@@ -2,6 +2,7 @@ from datasets import Dataset
 import datasets
 import os
 import uuid
+import json
 
 from packages.utils.globals import DATASETS_PATH
 from packages.utils.utils_func import get_token
@@ -9,6 +10,10 @@ from packages.utils.utils_func import get_token
 
 
 def push_audio_dataset():
+    champion_list : list[str] = list()
+    with open(DATASETS_PATH + "champion_mapping.json", "r") as f:
+        champion_list = json.load(f)
+    
     audio_dataset_dict = {
         "audio": [],
         "label": [],
@@ -16,8 +21,15 @@ def push_audio_dataset():
     }
     for root, dirs, files in os.walk(DATASETS_PATH + "youtube/audio/"):
         for name in files:
+            label : str = ""
+            for champion in champion_list:
+                if champion in name:
+                    label = champion
+                    
+            if label == "":
+                label = "guide"
             audio_dataset_dict["audio"].append(os.path.join(root, name))
-            audio_dataset_dict["label"].append(name.split(' ')[2])
+            audio_dataset_dict["label"].append(label)
             audio_dataset_dict["id"].append(str(uuid.uuid4()))
             
     
