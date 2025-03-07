83532622e44468da2d4df8ca775a5fe73266d1ea
avinot244 <aymericvinot38@gmail.com>
2025-03-07 23:11:33 +0100

    discovering firecrawl and fixing token management

---
+++ services/api/firecrawl/api_calls_firecrawl.py
@@ -0,0 +1,9 @@
+from firecrawl import FirecrawlApp
+from packages.utils_func import get_token
+
+def scrape(url : str):
+    token = get_token("read", "firecrawl")
+    app = FirecrawlApp(api_key=token)
+    
+    scrape_result = app.scrape_url('firecrawl.dev', params={'formats': ['markdown']})
+    return scrape_result["markdown"]
--- services/create_database/create_wiki_database.py
+++ services/create_database/create_wiki_database.py
@@ -1,2 +1,6 @@
+from services.api.firecrawl.api_calls_firecrawl import scrape
+
 def create_wiki_database():
-    pass
+    result = scrape("")
+    encoded_result = result.encode("utf-8")
+    print(encoded_result.decode("utf-8"))
