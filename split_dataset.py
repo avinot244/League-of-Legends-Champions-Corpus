eb477c933a80375628b68fa462ad2802a15155b7
avinot244 <aymericvinot38@gmail.com>
2025-04-16 00:09:37 +0200

    update file paths for training and validation datasets in split_dataset.py

--- split_dataset.py
+++ split_dataset.py
@@ -13,11 +13,11 @@ with open("./data/fill-mask/v6/lol-champs.jsonl", "r") as f:
     validation_split : list[str] = lines[split:]
     
     
-    with open("./data/fill-mask/lol-champs-train.jsonl", "w") as f_t:
+    with open("./data/fill-mask/v6/lol-champs-train.jsonl", "w") as f_t:
         for line_t in train_split:
             f_t.write(line_t)
             
-    with open("./data/fill-mask/lol-champs-validation.jsonl", "w") as f_v:
+    with open("./data/fill-mask/v6/lol-champs-validation.jsonl", "w") as f_v:
         for line_v in validation_split:
             f_v.write(line_v)
             
