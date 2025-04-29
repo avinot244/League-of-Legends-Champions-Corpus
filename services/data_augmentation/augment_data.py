3f348f1e2a19bac0cf4fe36b14e4fc99c8c85516
avinot244 <aymericvinot38@gmail.com>
2025-04-29 20:26:20 +0200

    feat: add champion role augmentation and update augment_data function to support new mode

--- services/data_augmentation/augment_data.py
+++ services/data_augmentation/augment_data.py
@@ -9,6 +9,7 @@ from services.data_augmentation.champion_role_profile import champion_role_profi
 from services.data_augmentation.paraphrasing import paraphrase_text
 from services.data_augmentation.prompt_response import prompt_response
 from services.data_augmentation.champion_matchup import champion_matchup
+from services.data_augmentation.champion_role import champion_role
 from packages.globals import CHUNK_SIZE, CHUNK_OVERLAP, N_CHUNKS
 from transformers import AutoTokenizer
 
@@ -118,7 +119,7 @@ def champion_matchup_augmentation(output_path : str, error_path : str):
     
     with open(f"{output_path}mobalytics.jsonl", "r") as f:
         lines = f.readlines()
-        for champion in tqdm(champion_list[158:]):
+        for champion in tqdm(champion_list):
             time.sleep(10)
             for line in lines:
                 data = json.loads(line)
@@ -151,14 +152,39 @@ def champion_matchup_augmentation(output_path : str, error_path : str):
                     for d in augmented_data:
                         o.write(json.dumps(d) + "\n")
 
+def champion_role_augmentation(output_path : str, error_path : str):
+    champion_list : list[str] = list()
+    with open("./data/champion_mapping.json", "r") as c:
+        data : list[dict] = json.load(c)
+        champion_list = [d["name"] for d in data]
+        
+    with open(f"{output_path}mobalytics.jsonl", "r") as f:
+        lines = f.readlines()
+        for champion in tqdm(champion_list):
+            time.sleep(10)
+            for line in lines:
+                champion_card = get_champion_card(champion, output_path)
+                if champion_card != None:
+                    augmented_data : str = champion_role(champion_card, error_path)
+                    
+                    if os.path.exists(f"{output_path}augmented_data_role.jsonl"):
+                        open_mode = "a"
+                    else:
+                        open_mode = "w"
+                    
+                    with open(f"{output_path}augmented_data_role.jsonl", open_mode) as o:
+                        o.write(json.dumps({"id": str(uuid.uuid4()), "label": champion, "text": augmented_data}) + "\n")
+        
 
-def augment_data(output_path : str, error_path : str, mode : Literal["para/profile", "QA", "mu"]):
+def augment_data(output_path : str, error_path : str, mode : Literal["para/profile", "QA", "mu", "role"]):
     if mode == "para/profile":
         augment_data_with_prompt(output_path, error_path)
     elif mode == "QA":
         prompt_response_augmentation(output_path, error_path)
     elif mode == "mu":
         champion_matchup_augmentation(output_path, error_path)
+    elif mode == "role":
+        champion_role_augmentation(output_path, error_path)
     
     
     
---
+++ services/data_augmentation/champion_role.py
@@ -0,0 +1,32 @@
+from services.prompt_provider.prompt_provider import get_prompt
+from services.chat.anthropic_chat import chat_anthropic
+
+import json
+
+def champion_role(champion_context : str, error_path : str) -> str:
+    prompt : str = get_prompt("champion_role")
+    
+    prompt = prompt.replace("{{CHAMPION_CONTEXT}}", champion_context)
+    
+    try:
+        llm_output = chat_anthropic(prompt)
+        champion_role_description = llm_output.split("```markdown")[1].split("```")[0]
+    except Exception as e:
+        try:
+            with open(error_path, "r") as file:
+                error_data = json.load(file)
+        except (FileNotFoundError, json.JSONDecodeError):
+            error_data = {"ids": []}
+
+        # Add the id to the list of ids
+        if "ids" not in error_data:
+            error_data["ids"] = []
+        error_data["ids"].append(id)
+
+        # Save the updated error data back to the JSON file
+        with open(error_path, "w") as file:
+            json.dump(error_data, file, indent=4)
+
+        return None
+    
+    return champion_role_description
