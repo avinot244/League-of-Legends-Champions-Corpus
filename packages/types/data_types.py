59305a5b21d074ff83c8bf708bb76711f2fd8c2b
avinot244 <aymericvinot38@gmail.com>
2025-06-03 22:08:16 +0200

    refacto types

---
+++ packages/types/data_types.py
@@ -0,0 +1,5 @@
+from typing import Literal
+
+t_data_type = Literal["champions", "game_mechanics", "items", "runes", "game_mechanics", "summonner_spells", "classes", "html_to_markdown", "champion_card", "paraphrase", "matchup", "champion_role", "instruct_factual", "instruct_strategic", "instruct_role"]
+
+t_champion_features = Literal["damage_profile", "range", ""]
