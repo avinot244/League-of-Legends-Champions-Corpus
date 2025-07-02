36204d822939effc79ae90860ba889d48112ede9
avinot244 <aymericvinot38@gmail.com>
2025-07-02 13:13:53 +0200

    feat: improve champion_triplets function by refining JSON output extraction and enhancing error handling

--- services/data_augmentation/augmentation/champion_triplets.py
+++ services/data_augmentation/augmentation/champion_triplets.py
@@ -1,6 +1,9 @@
 from services.prompt_provider.prompt_provider import get_prompt
 from services.chat.anthropic_chat import chat_anthropic
 
+from anthropic._exceptions import OverloadedError
+import time
+
 import json
 
 def champion_triplets(
@@ -16,12 +19,18 @@ def champion_triplets(
     prompt = prompt.replace("{{NEGATIVE_DESCRIPTION}}", negative_description)
     prompt = prompt.replace("{{NUM_TRIPLETS}}", str(nb_triplets))
     
-    llm_output : str = chat_anthropic(prompt)
     
     try:
-        json_output = llm_output.split("```json")[1].strip().split("```")[0]
+        llm_output : str = chat_anthropic(prompt)
+        json_output = llm_output.split("<triplet_output>")[1].strip().split("</triplet_output>")[0]
         triplets = json.loads(json_output)
         return triplets
+    except OverloadedError as o:
+        print(f"{index} : Overload of AnthropicAPI")
+        time.sleep(60)
+        
+        
+    
     except Exception as e:
         print(f"{index} : Error generating champion triplets: {e} ")
 
